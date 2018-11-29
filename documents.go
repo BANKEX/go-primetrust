b319bb7b65fd9937d20ec692b6553dbb7c243e08
apechenkin <a.pechenkin@bankex.com>
2018-11-29 15:17:58 +0300

    add fix to upload documents

--- documents.go
+++ documents.go
@@ -3,9 +3,7 @@ package primetrust
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"github.com/BANKEX/go-primetrust/models"
 	"io"
 	"io/ioutil"
 	"log"
@@ -13,33 +11,27 @@ import (
 	"net/http"
 )
 
-func UploadDocument(document models.Document) (*models.DocumentResponse, error) {
+func UploadDocument(file multipart.File, filename string, accountId string, contactId string, description string, extension string, label string, mimeType string) (*map[string]interface{}, error) {
 	apiUrl := fmt.Sprintf("%s/uploaded-documents", _apiPrefix)
-
-	jsonBytes, err := json.Marshal(document)
-	if err != nil {
-		return nil, err
-	}
 
 	var b bytes.Buffer
 	w := multipart.NewWriter(&b)
 
-	fw, err := w.CreateFormFile("file", document.Label)
+	fw, err := w.CreateFormFile("file", filename)
 	if err != nil {
 		return nil, err
 	}
-	if _, err = io.Copy(fw, document.File); err != nil {
+	if _, err = io.Copy(fw, file); err != nil {
 		return nil, err
 	}
 
 	data := map[string]interface{}{
-		"account-id":  document.AccountID,
-		"contact-id":  document.ContactID,
-		"description": document.Description,
-		"extension":   document.Extension,
-		"label":       document.Label,
-		"mime_type":   document.MimeType,
-		"public":      document.Public,
+		"account-id":  accountId,
+		"contact-id":  contactId,
+		"description": description,
+		"extension":   extension,
+		"label":       label,
+		"mime_type":   mimeType,
 	}
 
 	for key, val := range data {
@@ -58,30 +50,32 @@ func UploadDocument(document models.Document) (*models.DocumentResponse, error) 
 
 	w.Close()
 
-	buffer := bytes.NewBuffer(jsonBytes)
-	log.Println(buffer)
 	req, err := http.NewRequest("POST", apiUrl, &b)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Content-Type", w.FormDataContentType())
+	req.Header.Set("Content-Type", w.FormDataContentType())
 	req.Header.Add("Authorization", _authHeader)
 
+	log.Println("make request")
 	client := &http.Client{}
+
 	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
+
 	defer res.Body.Close()
+
+	log.Printf("result  ", res)
 	if res.StatusCode != http.StatusOK {
-		return nil, errors.New(res.Status)
+		return nil, err
 	}
+
 	body, _ := ioutil.ReadAll(res.Body)
 
-	response := models.DocumentResponse{}
-	if err := json.Unmarshal(body, &response); err != nil {
-		return nil, errors.New("Unmarshal error")
-	}
+	var resData map[string]interface{}
+	log.Printf("result  ", body)
 
-	return &response, nil
+	if err := json.Unmarshal(body, &resData); err != nil {
+		return nil, err
+	}
+	return &resData, nil
 }
--- examples/documents/upload/main.go
+++ examples/documents/upload/main.go
@@ -2,33 +2,53 @@ package main
 
 import (
 	"github.com/BANKEX/go-primetrust"
-	"github.com/BANKEX/go-primetrust/models"
 	"log"
 	"os"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func main() {
 	primetrust.Init(true, os.Getenv("PRIMETRUST_LOGIN"), os.Getenv("PRIMETRUST_PASSWORD"))
 
+	r := mux.NewRouter()
+
+	r.HandleFunc("/upload", UploadDocumentHandler)
+
+	panic(http.ListenAndServe(":7000",r))
+}
+
+func UploadDocumentHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(w, r)
+
+	r.ParseMultipartForm(32 << 20)
+
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "File upload error. Please ensure that size of image file is less than 5 MB", http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
 	accountId := os.Getenv("PRIMETRUST_ACCOUNT")
 	contactId := os.Getenv("PRIMETRUST_CONTACT")
-	//file,_:= multipart.FileHeader{}("
-	document := models.Document{
-		AccountID:   accountId,
-		ContactID:   contactId,
-		Description: "Test upload for documentation",
-		File:        nil,
-		Extension:   ".jpg",
-		Label:       "Test document",
-		MimeType:    "image/jpeg",
-		Public:      false,
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Wrong issue date format. Please use YYYY-MM-DD (eg. 2010-12-25)", http.StatusInternalServerError)
+		return
 	}
 
-	if _, err := primetrust.UploadDocument(document); err != nil {
+	log.Println("Send request")
+	if response, err := primetrust.UploadDocument(file, fileHeader.Filename, accountId,contactId,r.PostForm.Get("description"),r.PostForm.Get("extention"),r.PostForm.Get("label"),r.PostForm.Get("mimeType")); err != nil {
 		log.Println("Error uploading new document:", err)
+		http.Error(w, "Error uploading new document:", http.StatusInternalServerError)
 	} else {
-		log.Println("Document uploaded OK")
+		log.Println("Document uploaded: ", response)
+		w.Write([]byte("OK"))
 	}
 
-	log.Println("Done")
+	return
 }
